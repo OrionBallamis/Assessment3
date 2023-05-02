d73938a0946b6dc51b1bb8210cdb6ac8eb6c0f61
Orion Ballamis <oriondragon10@gmail.com>
2023-05-02 16:38:23 -0400

    update

--- about.js
+++ about.js
@@ -5,10 +5,20 @@ console.log("hello world");
 function handleSubmit(evt) {
 	evt.preventDefault();
 	
-	console.log('form submit');
+	alert('form submit');
 }
 
 
+let image = document.querySelector('img');
 let form = document.querySelector('#contact');
+let color = document.getElementById('color');
+let place = document.getElementById('place');
+let ritual = document.getElementById('ritual');
 
+
+image.addEventListener('mouseover', () => alert('Hello'))
 form.addEventListener('submit', handleSubmit);
+color.addEventListener('click', () => alert('Red'));
+place.addEventListener('click', () => alert('My bed'));
+ritual.addEventListener('click', () => alert('In the morning I will hit the snooze button like a boss'));
+
